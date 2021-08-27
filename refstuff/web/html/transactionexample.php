27edd0b772ea1b71826317ae77913e403f21f7aa
Pieter van den Hombergh (homberghp) <p.vandenhombergh@fontys.nl>
2021-08-27 12:03:52 +0200

    simplified table

--- refstuff/web/html/transactionexample.php
+++ refstuff/web/html/transactionexample.php
@@ -13,18 +13,18 @@ $ds = (new PDODataSource("hotel_california"))->setHost('db');
 $conn = $ds->getConnection();
 
 $makeReservationQuery = <<<'SQL'
-with inp as ( 
-         select ?::date start_reservation,?::date end_reservation, ?::integer as item_id, ?::integer as customer_id 
+with inp as (
+         select ?::date start_reservation,?::date end_reservation, ?::integer as item_id, ?::integer as customer_id
      ),
-     validres as ( 
-         select start_reservation cstart_reservation, greatest(start_reservation+1,end_reservation) cend_reservation 
+     validres as (
+         select start_reservation cstart_reservation, greatest(start_reservation+1,end_reservation) cend_reservation
           from inp
      ),
      cost as (
-          select item_cost_per_day*(cend_reservation - cstart_reservation) rcost 
+          select item_cost_per_day*(cend_reservation - cstart_reservation) rcost
           from validres,rental_items join inp on (inp.item_id=rental_items.item_id)
      ),
-     makeres as ( 
+     makeres as (
           insert into reservations (during, item_id, for_customer,reservation_cost)
           select daterange(cstart_reservation,cend_reservation), item_id, customer_id, rcost
           from inp,validres,cost
@@ -51,7 +51,8 @@ SQL;
  * @param PDOConnection $conn
  * @param array $reservationParams
  */
-function makeReservation(PDO $conn, array $reservationParams) {
+function makeReservation(PDO $conn, array $reservationParams) : string {
+   $result='';
     global $makeReservationQuery, $queryText2, $queryText3;
     try {
         $conn->beginTransaction();
@@ -65,15 +66,16 @@ function makeReservation(PDO $conn, array $reservationParams) {
             $reservationParams['account'],
         ]);
 
-        printResultset($stmt, 'updated customer record');
+        $result .=resultsetToString($stmt, 'updated customer record');
         $conn->commit();
-        print_simple_table($conn, $queryText3, [$reservationParams['account']], 'transactions');
+        $result .= simpleTableToString($conn, $queryText3, [$reservationParams['account']], 'transactions');
     } catch (PDOException $ex) {
-        echo "<div class='errors'><h3 >transaction failed with</h3>
+        $results= "<div class='errors'><h3 >transaction failed with</h3>
               <pre >" . $ex->getMessage() . "</pre>
              </div>";
         $conn->rollBack();
     }
+    return $result;
 }
 
 // phpinfo(INFO_VARIABLES);
@@ -83,7 +85,16 @@ $start_date = filter_var($dates[0], FILTER_VALIDATE_REGEXP, $regex_filter);
 $end_date   = filter_var($dates[1], FILTER_VALIDATE_REGEXP, $regex_filter);
 $item= filter_input(INPUT_POST,'item',FILTER_SANITIZE_NUMBER_INT);
 $customer= filter_input(INPUT_POST,'customer',FILTER_SANITIZE_NUMBER_INT);
+$customerTable= simpleTableToString($conn,"select * from customers order by 1", [], 'customers');
+$reservationResult= makeReservation($conn, ['date_from' => $start_date,
+    'date_to' => $end_date, 'item' => $item, 'account' => $customer]);
+$queryText4 = <<<'SQL'
+select * from reservations r join customers c on(c.customer_id=r.for_customer) join  rental_items using(item_id)
+    order by 2 desc
+SQL;
+$reservationForUser= simpleTableToString($conn, $queryText4, [],'reservations');
 
+// no more php logic or method calls below this line
 ?>
 <!DOCTYPE html>
 <html>
@@ -97,18 +108,9 @@ $customer= filter_input(INPUT_POST,'customer',FILTER_SANITIZE_NUMBER_INT);
     </head>
     <body>
         <h1>My Ugly transaction stats page</h1>
-<?php
-print_simple_table($conn, "select * from customers order by 1", [], 'customers');
-
-makeReservation($conn, ['date_from' => $start_date,
-'date_to' => $end_date, 'item' => $item, 'account' => $customer]);
-//echo "<pre>$queryText3</pre>";
-$queryText4 = <<<'SQL'
-select * from reservations r join customers c on(c.customer_id=r.for_customer) join  rental_items using(item_id)
-    order by 2 desc
-SQL;
-print_simple_table($conn, $queryText4, [],'reservations');
-?>
+<?=$customerTable;?>
+<?=$reservationResult;?>
+<?=$reservationForUser;?>
 <a href='reservation.php'> back to reservations</a>
 </body>
 </html>
